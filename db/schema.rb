9c21dc8134545e828bf69d383e9a76fed565856a
cinq3 <cinqmars@nifty.com>
2015-10-11 08:57:57 +0000

    create messsage model

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20151009203848) do
 
   create_table "messages", force: :cascade do |t|
-    t.string   "neme"
+    t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

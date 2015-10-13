2c649b5d15caf3502c95f896907ed5ba0b214942
cinq3 <cinqmars@nifty.com>
2015-10-13 10:53:27 +0000

    Gemfile update for heroku 3rd

--- db/migrate/20151009203848_create_messages.rb
+++ db/migrate/20151009203848_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.string :neme
+      t.string :name
       t.string :body
 
       t.timestamps null: false

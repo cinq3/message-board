1929780549063fbbdd09c87c1d4974f3a013bd2f
cinq3 <cinqmars@nifty.com>
2015-10-16 05:58:16 +0000

    add age field 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009203848) do
+ActiveRecord::Schema.define(version: 20151016034927) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
